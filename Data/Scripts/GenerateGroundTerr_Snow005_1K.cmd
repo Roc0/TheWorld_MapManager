a33ec8f79294ea06724b12bcd9a48a7fd8792f5b
Riccardo Marini <riccardo.marini65@gmail.com>
2023-03-29 15:45:26 +0200

    wip

---
+++ Data/Scripts/GenerateGroundTerr_Snow005_1K.cmd
@@ -0,0 +1,6 @@
+@echo on
+set curpath=%~dp0
+cd %curpath%
+cd ..\..\..\ConsoleMapManager\x64
+ConsoleMapManager.exe GenerateGroundImage D:\TheWorld\Client\TheWorld_MapManager\Data\Ground\Snow005_1K Snow005_1K 512 D:\TheWorld\Client\TheWorld_MapManager\Data\Ground\Snow005_1K\Snow005_1K_Color.png D:\TheWorld\Client\TheWorld_MapManager\Data\Ground\Snow005_1K\Snow005_1K_Displacement.png D:\TheWorld\Client\TheWorld_MapManager\Data\Ground\Snow005_1K\Snow005_1K_NormalGL.png D:\TheWorld\Client\TheWorld_MapManager\Data\Ground\Snow005_1K\Snow005_1K_Roughness.png
+pause
